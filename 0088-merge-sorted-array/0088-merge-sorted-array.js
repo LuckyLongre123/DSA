0a73644924b9702cc264b4298ce1b0215ee712b6
Lucky Longre <officialluckylongre@gmail.com>
2026-07-14 14:52:40 +0530

    Time: 0 ms (100%), Space: 54.4 MB (45.1%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -11,18 +11,36 @@ var merge = function (nums1, m, nums2, n) {
     // nums1.sort((a, b) => a - b)
 
     // 2nd sol
-    let copy = [...nums1];
+    // let copy = [...nums1];
 
-    let i = 0, j = 0, a = 0;
-    while (i < m && j < n) {
-        console.log(copy[i], nums2[j])
-        if (copy[i] <= nums2[j]) nums1[a++] = copy[i++]
-        else
-            nums1[a++] = nums2[j++]
+    // let i = 0, j = 0, a = 0;
+    // while (i < m && j < n) {
+    //     console.log(copy[i], nums2[j])
+    //     if (copy[i] <= nums2[j]) nums1[a++] = copy[i++]
+    //     else
+    //         nums1[a++] = nums2[j++]
+    // }
+    // while(i < m)
+    // nums1[a++] = copy[i++];
+    // while(j < n)
+    // nums1[a++] = nums2[j++];
+
+    // 3rd sol
+    let i = m - 1;        
+    let j = n - 1;       
+    let a = m + n - 1;    
+
+     
+    while (i >= 0 && j >= 0) {
+        if (nums1[i] < nums2[j]) {
+            nums1[a--] = nums2[j--];
+        } else {
+            nums1[a--] = nums1[i--];
+        }
     }
-    while(i < m)
-    nums1[a++] = copy[i++];
-    while(j < n)
-    nums1[a++] = nums2[j++];
 
+     
+    while (j >= 0) {
+        nums1[a--] = nums2[j--];
+    }
 };
