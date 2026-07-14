3c4616ca3278ec85b9ace84aef09c33fd826d57c
Lucky Longre <officialluckylongre@gmail.com>
2026-07-14 16:46:18 +0530

    Time: 2 ms (58.34%), Space: 67.5 MB (17.02%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function (nums, k) {
+    let tm = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        tm[(i + k) % nums.length] = nums[i];
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        nums[i] = tm[i]
+    }
+
+};
