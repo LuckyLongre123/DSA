28d55246c068415bdc3380d3b573ea24d95484fc
Lucky Longre <officialluckylongre@gmail.com>
2026-07-15 12:21:19 +0530

    Time: 0 ms (100%), Space: 57.6 MB (64.72%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+    let write = 0;
+
+    for (let read = 1; read < nums.length; read++)
+    if (nums[read] !== nums[write])
+        nums[++write] = nums[read];
+    return write + 1;
+
+};
